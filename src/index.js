81b91585e8461d3ee25727e0a79215ba4078261a
midhunrajk <midhunrajk009@gmail.com>
2023-08-02 18:39:52 +0530

    changing css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import "./index.css"
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(<App />);
